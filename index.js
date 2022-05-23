495394572e8c3cf0621760c309ddea2530b897ee
25garcial <91680716+25garcial@users.noreply.github.com>
2022-05-23 13:28:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-for(var i=0; i<10; i++){
-    var spot=document.createElement("div")
-    spot.classList.add("x")
-    document.body.apendChild(spot)
+for (var i = 0; i < 10; i++) {
+  var spot = document.createElement("div")
+  spot.classList.add("columns")
+  document.body.appendChild(spot)
 }
-for(var i=0; i<10; i++){
-    var spot=document.createElement("div")
-    spot.classList.add("y")
-    spot.addEventListener("click", function(){this.textContent=""};document.body.querySelectorAll(".y") )
-    document.body.querySelectorAll(".x")[i].appendChild(spot)
+for (var i = 0; i < 10; i++) {
+  for (var o = 0; o < 10; o++) {
+    let spot = document.createElement("div")
+    spot.classList.add("spot")
+    spot.addEventListener("click", function() {
+      console.log("triggered")
+      if (this.textContent == "click me") {
+        this.textContent = "";
+        console.log("succeded")
+        let random = Math.floor(Math.random() * 100)
+        document.querySelectorAll(".spot")[random].textContent = "click me"
+        console.log(random)
+      }
+    })
+    document.querySelectorAll(".columns")[i].appendChild(spot)
+  }
 }
-spot.textContent="click me"
+document.querySelectorAll(".spot")[Math.floor(Math.random()) * 100].textContent = "click me"
