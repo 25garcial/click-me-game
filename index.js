bffb7189b27d62a889f844a279e991182ee2ad8a
25garcial <91680716+25garcial@users.noreply.github.com>
2022-05-23 10:40:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ for(var i=0; i<10; i++){
 for(var i=0; i<10; i++){
     var spot=document.createElement("div")
     spot.classList.add("y")
+    spot.addEventListener("click", function(){this.textContent=""};document.body.querySelectorAll(".y") )
     document.body.querySelectorAll(".x")[i].appendChild(spot)
 }
+spot.textContent="click me"
