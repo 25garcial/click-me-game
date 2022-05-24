9e8f8e60428b6aaa1375bda379651c7b480dec59
25garcial <91680716+25garcial@users.noreply.github.com>
2022-05-24 10:13:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-
 for (var i = 0; i < 100; i++) {
-    let spot = document.createElement("div")
-    spot.classList.add("spot")
-    spot.addEventListener("click", function() {
-      console.log("triggered")
-      if (this.textContent == "click me") {
-        this.textContent = "";
-        console.log("succeded")
-        let random = Math.floor(Math.random() * 100)
-        document.querySelectorAll(".spot")[random].textContent = "click me"
-        console.log(random)
-      }
-    })
-    document.querySelector(".container").appendChild(spot)
-  
+  let spot = document.createElement("div")
+  spot.classList.add("spot")
+  spot.addEventListener("click", function() {
+    console.log("triggered")
+    if (this.textContent == "click me") {
+      this.textContent = "";
+      console.log("succeded")
+      let random = Math.floor(Math.random() * 100)
+      document.querySelectorAll(".spot")[random].textContent = "click me"
+      console.log(random)
+    }
+  })
+  document.querySelector(".container").appendChild(spot)
+
 }
 document.querySelectorAll(".spot")[Math.floor(Math.random()) * 100].textContent = "click me"
