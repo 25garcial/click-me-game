a1e85d60b5c31a6fb11da18deb7df069887cd0a7
25garcial <91680716+25garcial@users.noreply.github.com>
2022-05-24 10:31:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+let score = 0
 for (var i = 0; i < 100; i++) {
+
   let spot = document.createElement("div")
   spot.classList.add("spot")
   spot.addEventListener("click", function() {
@@ -6,6 +8,8 @@ for (var i = 0; i < 100; i++) {
     if (this.textContent == "click me") {
       this.textContent = "";
       console.log("succeded")
+      score++
+      document.querySelector(".score").textContent = score
       let random = Math.floor(Math.random() * 100)
       document.querySelectorAll(".spot")[random].textContent = "click me"
       console.log(random)
